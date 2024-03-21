fa4f8e5eda3805111f02ed25933b0ec02fb6a240
Antonyjin <antony.jin@epitech.eu>
2024-03-21 15:08:15 +0100

    Fix: Directory test was not in a good place
    Solution: Put the directory test at the root of the repo

--- test/python_test.py
+++ test/python_test.py
@@ -4,7 +4,7 @@ from mktestdocs import check_md_file
 
 
 @pytest.mark.parametrize(
-    "fpath", pathlib.Path("../libraries/python-sdk").glob("**/*.md"), ids=str
+    "fpath", pathlib.Path("../docs/libraries/python-sdk").glob("**/*.md"), ids=str
 )
 def test_run_python_code(fpath):
     check_md_file(fpath=fpath)
