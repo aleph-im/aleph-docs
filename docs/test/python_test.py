de6f14700365a13291ed16af9a532056d5a49167
Antonyjin <antony.jin@epitech.eu>
2024-03-25 22:02:20 +0100

    Fix: Missing path and incorrect relative path Path to documentation in nodes wass missing. Relative path in nodes/metrics was incorrect
    Solution: Adding the path in the yml and fix the relative path

--- docs/test/python_test.py
+++
@@ -1,8 +0,0 @@
-import pathlib
-import pytest
-from mktestdocs import check_md_file
-
-
-@pytest.mark.parametrize('fpath', pathlib.Path("../libraries/python-sdk").glob("**/*.md"), ids=str)
-def test_run_python_code(fpath):
-    check_md_file(fpath=fpath)
