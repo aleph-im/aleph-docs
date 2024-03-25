a48c99faf2ee8c267da3908045bcab8fb6ee24ba
Hugo Herter <git@hugoherter.com>
2024-03-25 11:27:25 +0100

    Fix: Relative path failed when wrong current directory
    Using a relative path to refer to the `python-sdk` markdown files only worked from within the `/tests` directory.
    
    Solution: Use an absolute path to the target directory by referencing its location relative to the test file using `__file__`.

--- test/python_test.py
+++ test/python_test.py
@@ -1,10 +1,13 @@
-import pathlib
+from pathlib import Path
+
 import pytest
 from mktestdocs import check_md_file
 
+PYTHON_CODE_DIRECTORY = Path(__file__).parent / "../docs/libraries/python-sdk"
+
 
 @pytest.mark.parametrize(
-    "fpath", pathlib.Path("../docs/libraries/python-sdk").glob("**/*.md"), ids=str
+    "fpath", PYTHON_CODE_DIRECTORY.glob("**/*.md"), ids=str
 )
 def test_run_python_code(fpath):
     check_md_file(fpath=fpath)
