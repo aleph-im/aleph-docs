45df4ed9a94b1e107bfd85ef356adc0597db547f
Antonyjin <antony.jin@epitech.eu>
2024-03-25 14:25:01 +0100

    Fix: black check failed in test file
    Solution: run black and fix the issue.

--- test/python_test.py
+++ test/python_test.py
@@ -6,8 +6,6 @@ from mktestdocs import check_md_file
 PYTHON_CODE_DIRECTORY = Path(__file__).parent / "../docs/libraries/python-sdk"
 
 
-@pytest.mark.parametrize(
-    "fpath", PYTHON_CODE_DIRECTORY.glob("**/*.md"), ids=str
-)
+@pytest.mark.parametrize("fpath", PYTHON_CODE_DIRECTORY.glob("**/*.md"), ids=str)
 def test_run_python_code(fpath):
     check_md_file(fpath=fpath)
