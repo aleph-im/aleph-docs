d19237f5c7981b8e565496848a837e0f703939a3
Olivier Le Thanh Duong <olivier@lethanh.be>
2024-04-25 10:42:30 +0200

    Problem: Dev were not sure which version was deployed
    Solution: Calculate a version number based on the git commit and display it on the page

---
+++ version_hook.py
@@ -0,0 +1,12 @@
+import subprocess
+
+
+def on_config(config):
+    try:
+        output = subprocess.check_output("git describe --always --dirty", shell=True)
+        version = output.decode("utf8").strip()
+    except subprocess.CalledProcessError:
+        version = "local-dev"
+
+    config.setdefault("extra", {})["version"] = version
+    return config
